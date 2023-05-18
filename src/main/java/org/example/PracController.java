61bd4dde920662e47e87658e202c5f9f88924ca0
soyeon <luora@naver.com>
2023-05-18 23:38:12 +0900

    [add] file

--- src/main/java/org/example/PracController.java
+++ src/main/java/org/example/PracController.java
@@ -1,16 +1,24 @@
 package org.example;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class PracController {
+	@Value("${hello.world}")
+	private String helloWorld;
 
 	@GetMapping("/hello")
 	public String hello(){
-		return "hello";
+		return "helloWorld";
+	}
+
+	@GetMapping("/hello2")
+	public String getHelloWorld(){
+		return helloWorld;
 	}
 
 }
